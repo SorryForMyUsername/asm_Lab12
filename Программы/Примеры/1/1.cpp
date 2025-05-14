91e9f75712f11e77994875b3816074f1899594c8
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-14 14:38:20 +0300

    Add files via upload

---
+++ Программы/Примеры/1/1.cpp
@@ -0,0 +1,26 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main()
+{
+    double a = 1.1;
+    __asm {
+        fld1
+        fldz
+        fld a
+        fcom st(1)
+        fstsw ax
+        sahf
+        jb l1
+
+        fadd st(0), st(2)
+        jmp p1
+    l1:
+        fabs
+    p1:
+        fstp a
+    }
+    printf("\t a==> %g\n",a);
+    _getch();
+    return 0;
+}
---
+++ Программы/Примеры/2/2.cpp
@@ -0,0 +1,41 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main()
+{
+    double x = 1, y = 6, z = 1, min, f;
+    __asm {
+        fld z
+        fld y
+        fld x
+        fadd st(0), st(1)
+        fld1
+        fadd st(0), st(0)
+        fdiv st(3), st(0)
+        fxch st(3)
+        fadd st(0), st(1)
+        fld z
+        fld x
+        fmul st(0), st(4)
+        fmul st(0), st(1)
+
+        fcom st(2)
+        fstsw ax
+        sahf
+        ja p1
+        fst min
+        jmp p3
+    p1:
+        fxch st(2)
+        fst min
+    p3:
+        fld1
+        fld min
+        fmul st(0), st(0)
+        fadd st(0), st(1)
+        fstp f
+    }
+    printf("\t min==> %g\n", f);
+    _getch();
+    return 0;
+}
