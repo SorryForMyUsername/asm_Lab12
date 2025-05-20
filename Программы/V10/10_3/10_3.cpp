47cabf1f65b759c399988908a18441c43f454a40
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-20 23:57:47 +0300

    Add files via upload

---
+++ Программы/V10/10_3/10_3.cpp
@@ -0,0 +1,50 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main()
+{
+	double x, y;
+	printf("x: ");
+	scanf_s("%lf", &x);
+
+	__asm {
+		fldz
+		fld x
+		fcomi st(0), st(1)
+		jae XisPositive
+		  fcos
+		  fmul st(0), st(0)
+		  fmul st(0), st(0)
+		  jmp asmEnd
+	XisPositive:
+		fld1
+		fld1
+		fadd st(0), st(1)
+		fdivp st(1), st(0)
+		fxch st(1)
+		fcomi st(0), st(1)
+		ja XgreaterThen05 
+		  f2xm1
+		  fld1
+		  fld1
+		  fadd st(0), st(1)
+		  fadd st(1), st(0)
+		  fmulp st(1), st(0)
+		  fsubp st(1), st(0)
+		  jmp asmEnd
+	XgreaterThen05:
+		fld1
+		fld st(1)
+		fmul st(0), st(0)
+		fadd st(0), st(1)
+		fxch st(1)
+		fsubp st(2), st(0)
+		fmul st(0), st(1)
+	asmEnd:
+		fstp y
+	}
+
+	printf("\nf(x) = %lf", y);
+
+	return 0;
+}
