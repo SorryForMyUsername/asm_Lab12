7766b9395108d89349ef251d87d52a67eea6479c
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-30 08:00:30 +0300

    Add files via upload

---
+++ V10/10_1/10_1.cpp
@@ -0,0 +1,46 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main() 
+{
+	double x, y, z, max;
+	printf("x: ");
+	scanf_s("%lf", &x);
+	printf("y: ");
+	scanf_s("%lf", &y);
+	printf("z: ");
+	scanf_s("%lf", &z);
+
+	__asm {
+		fld x
+		fld y
+		fld z
+
+		fcom st(1)
+		fstsw ax
+		sahf
+		ja XorZ
+		  fxch st(1)
+		  fcom st(2)
+		  fstsw ax
+		  sahf
+		  jb Zmax
+		    fstp max
+			jmp asmEnd
+	XorZ:
+		fcom st(2)
+		fstsw ax
+		sahf
+		jb Zmax
+		  fstp max
+		  jmp asmEnd
+	Zmax:
+		fxch st(2)
+		fstp max
+	asmEnd:
+	}
+
+	printf("\nmax = %lf", max);
+
+	return 0;
+}
---
+++ V10/10_2/10_2.cpp
@@ -0,0 +1,70 @@
+﻿#include "stdio.h"
+#include "conio.h"
+
+int main()
+{
+	double arr[] = { 11.1445, 3.00124, 2.5235 };
+
+	int count = 0;
+
+	__asm {
+		mov esi, 0
+		mov ecx, 0
+
+		fild count
+		fld1
+		fld st(0)
+		fadd st(1), st(0)
+		fadd st(1), st(0)
+
+		fld arr[esi*8]
+		fcomi st(0), st(1)
+		jbe second
+		  fcomi st(0), st(2)
+		  jae second
+		    fxch st(3)
+			fadd st(0), st(1)
+			inc ecx
+			fxch st(3)
+	second:
+		fstp st(0)
+		inc esi
+		
+		fld arr[esi*8]
+		fcomi st(0), st(1)
+		jbe third
+		  fcomi st(0), st(2)
+		  jae third
+		    fxch st(3)
+		    fadd st(0), st(1)
+			fld arr[esi*8]
+			fstp arr[ecx*8]
+			inc ecx
+		    fxch st(3)
+	third:
+		fstp st(0)
+		inc esi
+
+		fld arr[esi * 8]
+		fcomi st(0), st(1)
+		jbe asmEnd
+		  fcomi st(0), st(2)
+		  jae asmEnd
+		    fxch st(3)
+		    fadd st(0), st(1)
+		    fld arr[esi * 8]
+		    fstp arr[ecx * 8]
+		    inc ecx
+		    fxch st(3)
+
+	asmEnd:
+		fxch st(3)
+		fistp count
+	}
+
+	for (int i = 0; i < count; i++) {
+		printf("\n %lf", arr[i]);
+	}
+
+	return 0;
+}
